4e3c624a80208d4761e34e0bf4fafa2d355ee207
AraaVegaCuevas <araceli.vega1406@gmail.com>
2026-03-16 21:28:34 -0300

    Carpeta de Javascript subida.

---
+++ js/dynamicFooter.js
@@ -0,0 +1,70 @@
+const footer = document.querySelector('footer');
+
+footer.innerHTML = 
+`
+        <div class="container-fluid footer">
+            <div class="container py-5 border-start-0 border-end-0">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-6 col-xl-4">
+                        <div class="footer-item">
+                            <a href="index.html" class="p-0 mb-3">
+                                <img src="img/logos/consorcio_letraimg.png" alt="Logo_Consorcio">
+                            </a>
+                            <p>
+                                Hemos unido nuestras empresas para poder abordar problemas medioambientales 
+                                a gran escala. Nuestras soluciones de limpieza medioambiental 
+                                hacen que el pavimentado sea rápido, eficiente, duradero como el cemento y de bajo coste.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-2">
+                        <div class="footer-item">
+                            <h4 class="text-white mb-4">Ecoterra</h4>
+                            <a href="ecoterra.html#sobre-nosotros"><i class="fas fa-angle-right me-2"></i> Sobre nosotros</a>
+                            <a href="ecoterra.html#nuestros-productos"><i class="fas fa-angle-right me-2"></i> Nuestros productos</a>
+                            <a href="ecoterra.html#por-que-elegirnos"><i class="fas fa-angle-right me-2"></i> Por qué elegirnos</a>
+                            <a href="ecoterra.html#nuestros-clientes"><i class="fas fa-angle-right me-2"></i> Nuestros clientes</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-2">
+                        <div class="footer-item">
+                            <h4 class="text-white mb-4">Blauemeer</h4>
+                            <a href="blauemeer.html#sobre-nosotros"><i class="fas fa-angle-right me-2"></i> Sobre nosotros</a>
+                            <a href="blauemeer.html#desafio-desguace"><i class="fas fa-angle-right me-2"></i> Desafío desguace</a>
+                            <a href="blauemeer.html#nuestro-trabajo"><i class="fas fa-angle-right me-2"></i> Nuestro trabajo</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-4">
+                        <div class="footer-item">
+                            <h4 class="text-white mb-4">¡Contáctanos!</h4>
+                            <div class="d-flex align-items-center">
+                                <i class="fa fa-phone-alt text-primary me-3"></i>
+                                <p class="text-white mb-0">+56 9 8192 0022</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-envelope text-primary me-3"></i>
+                                <p class="text-white mb-0">info@consorcioecoterra-blauemeer.com</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container-fluid copyright py-4">
+            <div class="container">
+                <div class="row g-4 align-items-center">
+                    <div class="col-md-6 text-center text-md-start mb-md-0">
+                        <span class="text-body"><a href="https://wa.link/2h60d9" class="border-bottom text-white"><i class="fas fa-copyright text-light me-2"></i>Araceli Vega Cuevas</a>, All right reserved.</span>
+                    </div>
+                    <div class="col-md-6 text-center text-md-end text-body">
+                        <!--/*** The author’s attribution link must remain intact in the template. ***/-->
+                        <!--/*** If you wish to remove this credit link, please purchase the Pro Version . ***/-->
+                        Designed By <a class="border-bottom text-white" href="https://htmlcodex.com">HTML Codex</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+`;
---
+++ js/main.js
@@ -0,0 +1,147 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // Hero Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        autoplay: false,
+        smartSpeed: 500,
+        dots: true,
+        loop: false,
+        nav : true,
+        mouseDrag: false,
+        touchDrag: false,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+    });
+
+
+    // attractions carousel
+    $(".workattr-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        autoplayTimeout: 16000,
+        center: false,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 1000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        mouseDrag: false,
+        touchDrag: false,
+        dots: true,
+        loop: true,
+        nav: false
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
+    // Youtube Video Popup
+    const YTPlayerOverlay = document.querySelector('.youtube-player-overlay');
+    const YTPlay = document.querySelector('.pulsating-play-btn');
+
+    YTPlay.addEventListener('click', () => {
+        YTPlayerOverlay.classList.add('active');
+        YTPlay.style.opacity = '0';
+    });
+
+    YTPlayerOverlay.addEventListener('click', () => {
+        YTPlayerOverlay.classList.remove('active');
+        YTPlay.style.opacity = '1';
+    });
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    /* Uncomment this if animations are disabled due Windows Performance settings
+
+    // Smooth scroll for internal links w/ hash (for, #nosotros)
+    $('a[href*="#"]').on('click', function (e) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000, 'easeInOutExpo');
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+     */
+
+
+})(jQuery);
+
